e8dcc7557259a79bc978a1b55326372533aa8698
ChaniMeng <brionymeng@gmail.com>
2023-05-20 09:42:21 +0000

    feat - add predict_and_assemble function

--- train_utils.py
+++ train_utils.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
 
 
 @tf.function
@@ -9,3 +11,27 @@ def train_augment_fn(patch, labels):
     patch = tf.image.random_flip_up_down(patch, seed=42)
     labels = tf.image.random_flip_up_down(labels, seed=42)
     return patch, labels
+
+
+def predict_and_assemble(pipeline, volume, mask, threshold, model):
+    test_a_ds = pipeline.make_iterated_data_generator(volume, mask)
+    locations = pipeline.list_all_locations(mask)
+    all_pred = np.zeros(mask.shape)
+    all_binary_pred = np.zeros(mask.shape)
+    pred = []
+
+    for i, patch in tqdm(enumerate(test_a_ds())):
+        # pred[i]: (128, 128, 1)
+        p = model.predict(np.expand_dims(patch, 0))[0]
+        pred.append(p)
+        c = locations[i]
+        l = c[0] - pipeline.patch_halfsize
+        r = c[0] + pipeline.patch_halfsize
+        t = c[1] + pipeline.patch_halfsize
+        b = c[1] - pipeline.patch_halfsize
+        p = p.squeeze()
+        all_pred[l:r, b:t] = p
+        p = p > threshold
+        all_binary_pred[l:r, b:t] = p
+
+    return all_pred, all_binary_pred, pred
